13c62adb20eafba011107b33e10e7ecc5a1572e9
vmakhaev <vmakhaev@gmail.com>
2015-09-24 23:28:55 -0400

    nested fields and more tests

--- test/Doc.mocha.js
+++ test/Doc.mocha.js
@@ -14,6 +14,52 @@ describe('Doc', () => {
     assert.equal(doc.get('no_field'), undefined);
   });
 
+  it('should get field', () => {
+    let ops = [];
+    let lastDate = Date.now();
+
+    let op = {
+      id: 'id',
+      source: source,
+      type: 'add',
+      date: lastDate,
+      collectionName: collectionName,
+      docId: docId,
+      value: {
+        [field]: value
+      }
+    }
+    ops.push(op);
+
+    let doc = new Doc(docId, ops);
+
+    assert.equal(doc.get(field), value);
+  });
+
+  it('should get nested field', () => {
+    let ops = [];
+    let lastDate = Date.now();
+
+    let op = {
+      id: 'id',
+      source: source,
+      type: 'add',
+      date: lastDate,
+      collectionName: collectionName,
+      docId: docId,
+      value: {
+        nested: {
+          [field]: value
+        }
+      }
+    }
+    ops.push(op);
+
+    let doc = new Doc(docId, ops);
+
+    assert.equal(doc.get('nested.name'), value);
+  });
+
   it('should distillOps on same field', () => {
     let ops = [];
     let lastDate = Date.now();
@@ -87,6 +133,106 @@ describe('Doc', () => {
     assert.equal(Object.keys(doc.get()).length, 3);
   });
 
+  it('should distillOps on nested field', () => {
+    let ops = [];
+    let lastDate = Date.now();
+
+    let op = {
+      id: 'id',
+      source: source,
+      type: 'add',
+      date: lastDate,
+      collectionName: collectionName,
+      docId: docId,
+      value: {
+        nested: {
+          [field]: value
+        }
+      }
+    }
+    ops.push(op);
+
+    op = {
+      id: 'id1',
+      source: source,
+      type: 'set',
+      date: lastDate + 1,
+      collectionName: collectionName,
+      docId: docId,
+      field: 'nested.' + field,
+      value: 'Ivan'
+    }
+    ops.push(op);
+
+    op = {
+      id: 'id2',
+      source: source,
+      type: 'set',
+      date: lastDate + 2,
+      collectionName: collectionName,
+      docId: docId,
+      field: 'nested',
+      value: 'Ivan'
+    }
+    ops.push(op);
+
+    let doc = new Doc(docId, ops);
+    doc.distillOps();
+
+    assert.equal(doc.ops.length, 2);
+    assert.equal(doc.ops[1].date, lastDate + 2);
+  });
+
+  it('should not distillOps on nested field after field', () => {
+    let ops = [];
+    let lastDate = Date.now();
+
+    let op = {
+      id: 'id',
+      source: source,
+      type: 'add',
+      date: lastDate,
+      collectionName: collectionName,
+      docId: docId,
+      value: {
+        nested: {
+          [field]: value
+        }
+      }
+    }
+    ops.push(op);
+
+    op = {
+      id: 'id1',
+      source: source,
+      type: 'set',
+      date: lastDate + 1,
+      collectionName: collectionName,
+      docId: docId,
+      field: 'nested',
+      value: 'Ivan'
+    }
+    ops.push(op);
+
+    op = {
+      id: 'id2',
+      source: source,
+      type: 'set',
+      date: lastDate + 2,
+      collectionName: collectionName,
+      docId: docId,
+      field: 'nested.' + field,
+      value: 'Ivan'
+    }
+    ops.push(op);
+
+    let doc = new Doc(docId, ops);
+    doc.distillOps();
+
+    assert.equal(doc.ops.length, 3);
+    assert.equal(doc.ops[2].date, lastDate + 2);
+  });
+
   it('should refreshState on same field', () => {
     let ops = [];
 
@@ -109,15 +255,49 @@ describe('Doc', () => {
       collectionName: collectionName,
       docId: docId,
       field: field,
-      value: 'Ivan'
+      value: 'Vasya'
     }
     ops.push(op);
 
     let doc = new Doc(docId, ops);
-    //doc.refreshState();
 
     assert.equal(doc.get('_id'), docId);
-    assert.equal(doc.get(field), 'Ivan');
+    assert.equal(doc.get(field), 'Vasya');
+    assert.equal(Object.keys(doc.get()).length, 2);
+  });
+
+  it('should refreshState with nested field', () => {
+    let ops = [];
+
+    let op = {
+      source: source,
+      type: 'add',
+      date: Date.now(),
+      collectionName: collectionName,
+      docId: docId,
+      value: {
+        nested: {
+          [field]: value
+        }
+      }
+    }
+    ops.push(op);
+
+    op = {
+      source: source,
+      type: 'set',
+      date: Date.now(),
+      collectionName: collectionName,
+      docId: docId,
+      field: 'nested.' + field,
+      value: 'Vasya'
+    }
+    ops.push(op);
+
+    let doc = new Doc(docId, ops);
+
+    assert.equal(doc.get('_id'), docId);
+    assert.equal(doc.get('nested.' + field), 'Vasya');
     assert.equal(Object.keys(doc.get()).length, 2);
   });
 
@@ -150,6 +330,76 @@ describe('Doc', () => {
     assert.equal(doc.get('_id'), undefined);
     assert.equal(doc.get(field), undefined);
     assert.equal(doc.get(), undefined);
+  });
+
+  it('should refreshState with field del', () => {
+    let ops = [];
+
+    let op = {
+      source: source,
+      type: 'add',
+      date: Date.now(),
+      collectionName: collectionName,
+      docId: docId,
+      value: {
+        nested: {
+          [field]: value
+        }
+      }
+    }
+    ops.push(op);
+
+    op = {
+      source: source,
+      type: 'del',
+      date: Date.now(),
+      collectionName: collectionName,
+      docId: docId,
+      field: 'nested'
+    }
+    ops.push(op);
+
+    let doc = new Doc(docId, ops);
+
+    assert.equal(doc.get('_id'), docId);
+    assert.equal(doc.get('nested'), undefined);
+    assert.equal(Object.keys(doc.get()).length, 1);
+  });
+
+  it('should refreshState with nested field del', () => {
+    let ops = [];
+
+    let op = {
+      source: source,
+      type: 'add',
+      date: Date.now(),
+      collectionName: collectionName,
+      docId: docId,
+      value: {
+        nested: {
+          [field]: value
+        }
+      }
+    }
+    ops.push(op);
+
+    op = {
+      source: source,
+      type: 'del',
+      date: Date.now(),
+      collectionName: collectionName,
+      docId: docId,
+      field: 'nested.' + field
+    }
+    ops.push(op);
+
+    let doc = new Doc(docId, ops);
+
+    assert.equal(doc.get('_id'), docId);
+    assert.equal(typeof doc.get('nested'), 'object');
+    assert.equal(Object.keys(doc.get('nested')).length, 0);
+    assert.equal(doc.get('nested.' + field), undefined);
+    assert.equal(Object.keys(doc.get()).length, 2);
   });
 
   it('should getVersionFromOps from different sources', () => {
