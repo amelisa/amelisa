b8b698fa6164da4ac6bc4cedf68440da5dc75cb3
Vladimir Makhaev <vmakhaev@gmail.com>
2016-01-26 19:31:58 -0500

    refactor getModel

--- src/getModel.js
+++ src/getModel.js
@@ -5,6 +5,48 @@ import util from './util'
 
 let model
 
+async function initModel () {
+  await util.onDomReady()
+
+  model.unbundleLocalData()
+
+  let { clientStorage, collectionNames } = model.get('_app')
+
+  if (clientStorage) {
+    // Delete data of local collections to create them later with storage
+    delete model.collectionSet.data['_app']
+    delete model.collectionSet.data['_session']
+    let storage = new IndexedDbStorage(Array.from(collectionNames).concat(['_app', '_session']))
+    model.storage = storage
+    model.collectionSet.storage = storage
+    model.querySet.storage = storage
+
+    await storage.init()
+    await model.collectionSet.fillLocalCollectionsFromClientStorage()
+
+    model.unbundleLocalData()
+
+    let newProjectionHashes = model.get('_app.newProjectionHashes')
+    await model.onProjections(newProjectionHashes)
+  }
+
+  await model.init()
+
+  let source = model.get('_app.source')
+  if (!source) {
+    source = model.id()
+    model.source = source
+    model.set('_app.source', source)
+  } else {
+    model.source = source
+  }
+
+  let projectionHashes = model.get('_app.newProjectionHashes')
+  model.set('_app.projectionHashes', projectionHashes)
+
+  model.unbundleData()
+}
+
 function getModel () {
   if (model) return model
 
@@ -23,64 +65,10 @@ function getModel () {
 
   window.model = model
 
-  let storage
-
-  util
-    .onDomReady()
-    .then(() => {
-      model.unbundleLocalData()
-
-      let { clientStorage, collectionNames } = model.get('_app')
-
-      if (clientStorage) {
-        // Delete data of local collections to create them later with storage
-        delete model.collectionSet.data['_app']
-        delete model.collectionSet.data['_session']
-        storage = new IndexedDbStorage(Array.from(collectionNames).concat(['_app', '_session']))
-        model.storage = storage
-        model.collectionSet.storage = storage
-        model.querySet.storage = storage
-
-        return storage.init()
-      }
-
-      return Promise.resolve()
-    })
-    .then(() => {
-      if (!storage) return Promise.resolve()
-
-      return model.collectionSet.fillLocalCollectionsFromClientStorage()
-    })
-    .then(() => {
-      if (!storage) return Promise.resolve()
-
-      model.unbundleLocalData()
-
-      let newProjectionHashes = model.get('_app.newProjectionHashes')
-
-      return model.onProjections(newProjectionHashes)
-    })
-    .then(() => model.init())
-    .then(() => {
-      let source = model.get('_app.source')
-      if (!source) {
-        source = model.id()
-        model.source = source
-        model.set('_app.source', source)
-      } else {
-        model.source = source
-      }
-    })
-    .then(() => {
-      let projectionHashes = model.get('_app.newProjectionHashes')
-      return model.set('_app.projectionHashes', projectionHashes)
-    })
-    .then(() => {
-      model.unbundleData()
-      ws.open()
-    })
-    .catch((error) => {
-      console.error(error, error.stack)
+  initModel()
+    .then(() => ws.open())
+    .catch((err) => {
+      console.error(err, err.stack)
     })
 
   return model
