b760594e7e5b389218f48b5f84f131552727db44
Vladimir Makhaev <vmakhaev@gmail.com>
2016-03-11 22:34:04 +0300

    tests for string type

---
+++ test/types/StringType.test.js
@@ -0,0 +1,44 @@
+import assert from 'assert'
+import StringType from '../../src/types/StringType'
+
+let string
+
+describe('StringType', () => {
+  it('should have value empty string by default', () => {
+    string = new StringType()
+
+    assert.equal(string.get(), '')
+  })
+
+  it('should insert at empty string', () => {
+    string = new StringType()
+    string.insert(null, '1', 'a')
+
+    assert.equal(string.get(), 'a')
+  })
+
+  it('should insert by positionId', () => {
+    string = new StringType()
+    string.insert(null, '1', 'a')
+    string.insert('1', '2', 'b')
+
+    assert.equal(string.get(), 'ab')
+  })
+
+  it('should insert without positionId', () => {
+    string = new StringType()
+    string.insert(null, '1', 'a')
+    string.insert(null, '2', 'b')
+
+    assert.equal(string.get(), 'ba')
+  })
+
+  it('should remove', () => {
+    string = new StringType()
+    string.insert(null, '1', 'a')
+    string.insert('1', '2', 'b')
+    string.remove('2')
+
+    assert.equal(string.get(), 'a')
+  })
+})
