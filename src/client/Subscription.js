78f81d84ee3999a329d10d4dd886c2b519cd3209
Vladimir Makhaev <vmakhaev@gmail.com>
2016-04-06 20:49:18 +0300

    fix subscribe options in subscription

--- src/client/Subscription.js
+++ src/client/Subscription.js
@@ -69,7 +69,7 @@ class Subscription extends EventEmitter {
       subscribe.on('change', this.onChange)
 
       let subscribeOptions = this.subscribeOptionses[i]
-      subscribeOptions = Object.assign({}, this.options, subscribe.subscribeOptions)
+      subscribeOptions = Object.assign({}, this.options, subscribeOptions)
 
       let promise = subscribe.subscribe(subscribeOptions)
       promises.push(promise)
